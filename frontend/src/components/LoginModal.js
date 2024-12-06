a43175fc846bcbd3604d8ac56ee2582f33572c97
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2024-12-06 00:44:14 -0800

    Update LoginModal.js

--- frontend/src/components/LoginModal.js
+++ frontend/src/components/LoginModal.js
@@ -13,8 +13,8 @@ function LoginModal({ onLogin }) {
   return (
     <div style={{
       position:'fixed',
-      top:0, left:0,
-      width:'100%', height:'100%',
+      top:0,left:0,
+      width:'100%',height:'100%',
       background:'rgba(0,0,0,0.5)',
       display:'flex',justifyContent:'center',alignItems:'center'
     }}>
